0f4c866d6a0f2c01859dc620ef1c2db969f0837f
Solomon Eseme <kaperskyguru@gmail.com>
2022-12-26 18:49:53 +0100

    fix: error status error

--- pages/errors/500.vue
+++ pages/errors/500.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-if="error404">Page not found</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">Home page</NuxtLink>
   </div>
@@ -11,5 +11,11 @@ export default {
   name: 'ErroR404',
   layout: 'portfolio',
   props: ['error'], // you can set a custom layout for the error page
+
+  computed: {
+    error404() {
+      return this.error?.statusCode === 404
+    },
+  },
 }
 </script>
