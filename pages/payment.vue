b2028172ff9c8cdcaf61594cc7cfd0129f416351
Solomon Eseme <kaperskyguru@gmail.com>
2023-04-10 13:04:02 +0100

    Create payment.vue

---
+++ pages/payment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    You're been redirected to the payment page. If not,
+    <a href="#" @click.prevent="openPayment">click here.</a>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    Paddle.Setup({ vendor: Number(153088) })
+    Paddle.Checkout.open({ product: 822998 })
+  },
+  method: {
+    openPayment() {
+      Paddle.Checkout.open({ product: 822998 })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
